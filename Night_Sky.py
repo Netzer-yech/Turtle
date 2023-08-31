b9c0eece8be4de9280f70df0f4c32ce2cd7bdcf8
Netzer-yech <netzer.yech@gmail.com>
2023-08-31 07:14:04 +0300

    couple of turtles

--- Night_Sky.py
+++ Night_Sky.py
@@ -8,11 +8,11 @@ screen.title("Randomly Filled Stars")
 
 # Create a Turtle instance
 t1 = turtle.Turtle()
-# t2 = turtle.Turtle()
+t2 = turtle.Turtle()
 t1.speed(0)  # Fastest drawing speed
-# t2.speed(0)
+t2.speed(0)
 t1.color("white")
-# t2.color("white")
+t2.color("white")
 
 
 # Function to draw a star
@@ -20,39 +20,38 @@ def draw_star(turtle, size):
 
     for _ in range(10):
         t1.hideturtle()
+        t2.hideturtle()
         turtle.circle(size)  # Move forward by the given size
 
 
 # Draw random stars on the screen within the specified range
 for _ in range(10000):  # You can adjust the number of stars
     # Generate random coordinates within the specified range
-    x = random.randint(-370, 370)
-    y = random.randint(-350, 350)
+    x1 = random.randint(-370, 370)
+    y1 = random.randint(-350, 350)
+    x2 = random.randint(-370, 370)
+    y2 = random.randint(-350, 350)
 
     size1 = (random.randint(10, 20))/30  # Adjust the size range of stars
+    size2 = (random.randint(10, 40))/20  # Adjust the size range of stars
+
 
     t1.penup()  # Lift the pen to move without drawing
-    t1.goto(x, y)  # Move the turtle to the random position
+    t1.goto(x1, y1)  # Move the turtle to the random position
     t1.pendown()  # Lower the pen to start drawing
     t1.fillcolor("white")  # Set fill color to white
     t1.begin_fill()  # Begin filling the shape
     draw_star(t1, size1)  # Call the draw_star function with the chosen size
     t1.end_fill()  # End filling the shape
+    t2.penup()  # Lift the pen to move without drawing
+    t2.goto(x2, y2)  # Move the turtle to the random position
+    t2.pendown()  # Lower the pen to start drawing
+    t2.fillcolor("white")  # Set fill color to white
+    t2.begin_fill()  # Begin filling the shape
+    draw_star(t2, size2)  # Call the draw_star function with the chosen size
+    t2.end_fill()  # End filling the shape
 
-# for _ in range(10000):  # You can adjust the number of stars
-#     # Generate random coordinates within the specified range
-#     x1 = random.randint(-400, 400)
-#     y1 = random.randint(-400, 400)
-#
-#     size2 = (random.randint(10, 40))/20  # Adjust the size range of stars
-#
-#     t1.penup()  # Lift the pen to move without drawing
-#     t1.goto(x1, y1)  # Move the turtle to the random position
-#     t1.pendown()  # Lower the pen to start drawing
-#     t1.fillcolor("white")  # Set fill color to white
-#     t1.begin_fill()  # Begin filling the shape
-#     draw_star(t1, size2)  # Call the draw_star function with the chosen size
-#     t1.end_fill()  # End filling the shape
+
 
 # Close the turtle graphics window when clicked
 screen.exitonclick()
